fff61b3525c54129a87201e2fb7b8c1f9210837e
James Henager <jameschenager@gmail.com>
2020-06-08 14:59:18 -0700

    add formatting changes for code readability
    Co-Authored-by: Ben Russell <benjaminrussell36@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
+const cardNumbers = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+const cardSuits = [" of clubs", " of diamonds", " of spades", " of hearts"]
+
+
 $(document).ready(function() {
-  const cardNumbers = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
-  const cardSuits = [" of clubs", " of diamonds", " of spades", " of hearts"]
 
   cardSuits.forEach(function(cardSuit) {
     cardNumbers.forEach(function(cardNumber) {      
