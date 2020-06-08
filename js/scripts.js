7e4bb8fb7f27810529c7ba69bfe1c814633f375b
James Henager <jameschenager@gmail.com>
2020-06-08 14:50:13 -0700

    add for each functions to add two arrays together
    Co-Authored-by: Ben Russell <benjaminrussell36@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
+  const cardNumbers = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+  const cardSuits = [" of clubs", " of diamonds", " of spades", " of hearts"]
 
+  cardSuits.forEach(function(cardSuit) {
+    cardNumbers.forEach(function(cardNumber) {      
+      $("#card-list").append(
+        "<li>" + cardNumber + cardSuit + "</li>"
+      );
+    });
+  });
 });
